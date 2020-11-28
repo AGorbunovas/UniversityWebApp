9baea568698f1887860f7fc8e6fb52fbdbeec355
Andriejus Gorbunovas <a.gorbunovas@gmail.com>
2020-11-28 12:02:25 +0200

    Create DbInitializer

--- UniversityWebApplication/UniversityWebApplication/Data/AppDbContext.cs
+++ UniversityWebApplication/UniversityWebApplication/Data/AppDbContext.cs
@@ -16,8 +16,6 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options)
             : base(options)
     {
-        Database.EnsureDeleted();
-        Database.EnsureCreated();
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
---
+++ UniversityWebApplication/UniversityWebApplication/Data/DbInitializer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using UniversityWebApplication.Models;
+
+namespace UniversityWebApplication.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(AppDbContext context)
+        {
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            if (context.ToDoCategories.Any())
+            {
+                return;
+            }
+
+            var toDoCategory = new ToDoCategory[]
+                {
+                    new ToDoCategory
+                    {
+                        CategoryName = "Work"
+                    },
+                    new ToDoCategory
+                    {
+                        CategoryName = "Home"
+                    }
+                };
+            context.ToDoCategories.AddRange(toDoCategory);
+            context.SaveChanges();
+
+            var toDoList = new ToDo[]
+                {
+                    new ToDo
+                    {
+                        Description = "Peržiūrėti paštą",
+                        IsDone = false,
+                        ToDoCategory_Id = 1
+                    },
+                    new ToDo
+                    {
+                        Description = "Nueiti į parduotuvę",
+                        IsDone = false,
+                        ToDoCategory_Id = 2
+                    },
+                    new ToDo
+                    {
+                        Description = "Susitikimas su užsakovu",
+                        IsDone = true,
+                        ToDoCategory_Id = 1
+                    },
+                    new ToDo
+                    {
+                        Description = "Kelionė į kalnus",
+                        IsDone = true,
+                        ToDoCategory_Id = 2
+                    }
+                };
+            context.ToDos.AddRange(toDoList);
+            context.SaveChanges();
+        }
+    }
+}
