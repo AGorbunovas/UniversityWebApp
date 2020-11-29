e46ec234dc667e6bb0acce07a605617f0032345d
Andriejus Gorbunovas <a.gorbunovas@gmail.com>
2020-11-29 18:21:13 +0200

    Add repository interface

---
+++ UniversityWebApplication/UniversityWebApplication/Data/IAppDbRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using UniversityWebApplication.Models;
+
+namespace UniversityWebApplication.Data
+{
+    public interface IAppDbRepository
+    {
+        public IQueryable<ToDo> ToDos { get; }
+
+        public IQueryable<ToDoCategory> ToDoCategories { get; }
+
+        void Add<EntityType>(EntityType entity);
+
+        void SaveChanges();
+    }
+}
