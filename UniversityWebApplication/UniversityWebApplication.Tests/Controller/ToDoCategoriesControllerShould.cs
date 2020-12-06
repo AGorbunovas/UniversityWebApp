0fa2fff1a7e692bb7b46a59a9551da581416317e
Andriejus Gorbunovas <a.gorbunovas@gmail.com>
2020-12-06 19:04:24 +0200

    Create xUnit Test project, add InMemory repository, create Unit test

---
+++ UniversityWebApplication/UniversityWebApplication.Tests/Controller/ToDoCategoriesControllerShould.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UniversityWebApplication.Controllers;
+using UniversityWebApplication.Tests.Repository;
+using Xunit;
+
+namespace UniversityWebApplication.Tests.Controller
+{
+    public class ToDoCategoriesControllerShould
+    {
+        
+        private AppDbContext _context;
+        private ToDoCategoriesController _categoryController;
+
+        public ToDoCategoriesControllerShould()
+        {
+            _context = InMemoryAppDbContext.GetInMemoryAppDbContext();
+            _categoryController = new ToDoCategoriesController(_context);
+        }
+        
+
+        [Fact(DisplayName = "Index should return default view")]
+        public void IndexShouldReturnDefaultView()
+        {
+            var result = _categoryController.Index() as ViewResult;
+
+            Assert.NotNull(result);
+            Assert.True(string.IsNullOrEmpty(result.ViewName) || result.ViewName == "Index");
+        }
+    }
+}
