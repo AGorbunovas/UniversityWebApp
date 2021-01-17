3dffeb22ca13dbbfc949cd6c38f74e6ae3b046d9
Andriejus <andriejus.gorbunovas@tesonet.com>
2021-01-17 19:01:18 +0200

    Add IToDoService

---
+++ UniversityWebApplication/UniversityWebApplication/Services/IToDoService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UniversityWebApplication.Services
+{
+    public interface IToDoService
+    {
+        void AddToDos();
+
+        void EditToDos();
+
+        void DeleteToDos();
+    }
+}
